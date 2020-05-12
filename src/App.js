42e147aeaa60fadc2ef517d1e701029b981fa908
wei peluso <weipeluso@gmail.com>
2020-05-12 16:40:44 -0400

    finish stretch search

--- src/App.js
+++ src/App.js
@@ -4,20 +4,37 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState}from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 import PostsPage from "./components/PostsContainer/PostsPage"
 import SearchBar from "./components/SearchBar/SearchBarContainer"
+import data from  "./dummy-data"
 
 const App = () => {
+ 
+
+  const [posts, setPosts] = useState(data)
+  const [searchTerm, setSearchTerm] = useState('')
+
+
+  const filterPosts = posts => {
+    return posts.filter(postsObj => {
+      if (!searchTerm) {
+        return postsObj;
+      }
+      if (postsObj.username.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return postsObj
+      }
+    })
+  }
 
   return (
     <div className="App">
       {/* Add imported components here to render them */}
 
-      <SearchBar />
-      <PostsPage />
+      <SearchBar updateSearchTerm={setSearchTerm} />
+      <PostsPage posts={filterPosts(posts)}/>
     </div>
   );
 };
--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -5,7 +5,16 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons'
 import "./SearchBar.css";
 
-const SearchBar = () => {
+
+
+const SearchBar = (props) => {
+
+  const { updateSearchTerm } = props
+
+  const changeHandler = event => {
+    updateSearchTerm(event.target.value)
+  }
+
   return (
     <div className="search-bar-wrapper">
       <div className="social">
@@ -15,6 +24,7 @@ const SearchBar = () => {
         <input
           type="text"
           placeholder="Search"
+          onChange={changeHandler}
         />
       </form>
       <div className="social-wrapper">
