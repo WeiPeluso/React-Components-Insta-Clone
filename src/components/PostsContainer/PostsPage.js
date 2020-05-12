25479ef07ab272f93ff97e20d34a6aca847fffe1
wei peluso <weipeluso@gmail.com>
2020-05-12 15:16:37 -0400

    finished mvp and working on stretch search

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,26 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import data from  "../../dummy-data"
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+  const [posts,setPosts]=useState(data)
+  //console.log(posts)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {
+         posts.map((postObj,index)=>{
+
+            return <Post key={index} post={postObj} />
+         })
+
+      }
+  
     </div>
   );
 };
